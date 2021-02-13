5b1e4a51488ac2755334835353da971c08efdaf5
PrizrakNight <40437643+PrizrakNight@users.noreply.github.com>
2021-02-13 06:38:04 +0300

    gc

--- SimpleNotes.Server.Infrastructure/DependencyInjection.cs
+++ SimpleNotes.Server.Infrastructure/DependencyInjection.cs
@@ -2,8 +2,6 @@
 using Microsoft.Extensions.DependencyInjection;
 using SimpleNotes.Server.Domain.Contracts;
 using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace SimpleNotes.Server.Infrastructure
 {
