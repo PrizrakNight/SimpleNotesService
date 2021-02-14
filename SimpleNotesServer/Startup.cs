a78841cd235f6a5f3cca15a3048e32bc8f30ffd8
PrizrakNight <40437643+PrizrakNight@users.noreply.github.com>
2021-02-14 06:44:13 +0300

    Added Swagger support

--- SimpleNotesServer/Startup.cs
+++ SimpleNotesServer/Startup.cs
@@ -6,9 +6,11 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using SimpleNotes.Server.Application;
 using SimpleNotes.Server.Application.Options;
 using SimpleNotes.Server.Infrastructure;
+using Swashbuckle.AspNetCore.SwaggerGen;
 using System.Text;
 
 namespace SimpleNotesServer
@@ -37,6 +39,7 @@ namespace SimpleNotesServer
             services.AddDefaultInfrastructure(conf => conf.UseInMemoryDatabase("Test database"));
 
             services.AddControllers().AddJsonOptions(option => option.JsonSerializerOptions.IgnoreNullValues = true);
+            services.AddSwaggerGen(ConfigureSwaggerGen);
 
             services.AddAuthorization();
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
@@ -58,6 +61,34 @@ namespace SimpleNotesServer
                 });
         }
 
+        private void ConfigureSwaggerGen(SwaggerGenOptions obj)
+        {
+            obj.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                Name = "Bearer",
+                BearerFormat = "JWT",
+                Scheme = "bearer",
+                Description = "Specify the authorization token.",
+                In = ParameterLocation.Header,
+                Type = SecuritySchemeType.Http,
+            });
+
+            obj.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    new string[] { }
+                }
+            });
+        }
+
         private SymmetricSecurityKey GetSecurityKey(TokenProviderOptions options)
         {
             var appSecretBytes = Encoding.ASCII.GetBytes(options.ApplicationSecret);
@@ -70,6 +101,13 @@ namespace SimpleNotesServer
         {
             if (env.IsDevelopment())
                 app.UseDeveloperExceptionPage();
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "Simple Notes v1.0");
+            });
 
             app.UseHttpsRedirection();
             app.UseRouting();
